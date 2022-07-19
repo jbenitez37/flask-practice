50470cf8ac8dca05bd95e1ae629351feac98a2be
jbenitez37 <107884662+jbenitez37@users.noreply.github.com>
2022-07-19 09:31:24 -0700

    Add files via upload

--- tutorial.py
+++ tutorial.py
@@ -1,21 +1,58 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+import sqlalchemy
 
-app= Flask(__name__)
+app = Flask(__name__)
+app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(minutes=5)
 
-@app.route("/<name>")
+@app.route("/")
+def home():
+	return render_template("index.html")
 
-def home(name):
-  return render_template("index.html")
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+        flash("Login Succesful!")
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+          flash("Already Logged in!")
+          return redirect(url_for("user"))
 
-if __name__ == '__main__':
-  app.run()
+        return render_template("login.html")
+
+@app.route("/user", methods=["POST", "GET"])
+def user():
+    email = None
+    if "user" in session:
+        user = session["user"]
+
+        if request.method == "POST":
+          email = request.form["email"]
+          session["email"] = email
+        else:
+            if "email" in session:
+              email = session["email"]
+
+        return render_template("user.html", email=email)
+    else:
+        flash("You are not logged in!")
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+  flash("You have been logged out", "info")
+  session.pop("user", None)
+  session.pop("email", None)
+  return redirect(url_for("login"))
+
+if __name__ == "__main__":
+	app.run(debug=True)
 
 
 
-#@app.route("/<name>")
-#def user(name):
-    #return f"Hello {name}!"
 
-#@app.route("/admin/")
-#def admin():
-  #return redirect(url_for("user", name = "Admin!"))
