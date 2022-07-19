4c3c0d7baaca02325823a7168196b170f6e09a52
jbenitez37 <107884662+jbenitez37@users.noreply.github.com>
2022-07-19 15:00:50 -0700

    Add files via upload

--- tutorial.py
+++ tutorial.py
@@ -1,10 +1,27 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
-import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "hello"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(minutes=5)
+
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    
+    def __innit__(self, name, email):
+      self.name = name
+      self.email = email
+
+@app.route("/view")
+def view():
+    return render_template("view.html", values=Users.query.all())
 
 @app.route("/")
 def home():
@@ -16,6 +33,16 @@ def login():
         session.permanent = True
         user = request.form["nm"]
         session["user"] = user
+        found_user = Users.query.filter_by(name=user).delete()
+        for user in found_user:
+            user.delete()
+        if found_user: 
+            session["email"] = found_user.email
+        else:
+            usr = Users(user, "")
+            db.session.add(usr)
+            db.session.commit()
+            
         flash("Login Succesful!")
         return redirect(url_for("user"))
     else:
@@ -34,6 +61,10 @@ def user():
         if request.method == "POST":
           email = request.form["email"]
           session["email"] = email
+          found_user = Users.query.filter_by(name=user).first()
+          found_user.email = email
+          db.session.commit()
+          flash("Email was saved")
         else:
             if "email" in session:
               email = session["email"]
@@ -51,7 +82,8 @@ def logout():
   return redirect(url_for("login"))
 
 if __name__ == "__main__":
-	app.run(debug=True)
+  db.create_all()
+app.run(debug=True)
 
 
 
